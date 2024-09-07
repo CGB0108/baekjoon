baf48d6cc6fc4517ab0d7d5b668398f8444d8726
daengHee <lcs970108@gmail.com>
2024-09-07 21:07:31 +0900

    10773_제로

---
+++ src/class1/Pro10773.java
@@ -0,0 +1,36 @@
+package src.class1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pro10773 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] numArr = new int[N];
+        for (int i = 0; i < N; i++) {
+            int st = Integer.parseInt(br.readLine());
+            numArr[i] = st;
+
+        }
+
+        List<Integer> list = new ArrayList<>();
+        int sum = 0;
+        for (int num : numArr) {
+            if (num != 0) {
+                list.add(num);
+            } else {
+                list.remove(list.size() - 1);
+            }
+        }
+
+        for (int num : list) {
+            sum += num;
+        }
+
+        System.out.println(sum);
+    }
+}
